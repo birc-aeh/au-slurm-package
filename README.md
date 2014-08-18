056b88706a23116b13b7b92d41964334138544df
Anders Egerup Halager <aeh@birc.au.dk>
2014-08-18 11:27:38 +0200

    A little text about the config files

--- README.md
+++ README.md
@@ -8,8 +8,44 @@ Folder overview
 
     folder          install location        note
     ------          ----------------        ----
-    config/         /opt/slurm/etc/         Our configuration files
+    config/         /opt/slurm/etc/         Our configuration files (see further notes before installing)
     scripts/        /opt/slurm/scripts/     This folder holds the various prolog/epilog scripts
     replacements/   /opt/slurm/bin/         Replacements for most of our old tools
     tools/          /opt/slurm/bin/         New tools to make things nicer for the user
     support-bin/    various                 A few supporting programs
+
+Config files
+------------
+You need to pay attention to what you are installing on what machines here.
+`slurmdbd.conf` contains the user/pass for the database and should _only_ be
+installed on the controller machine(s).
+
+### slurm.conf
+This is the main config file that is needed on all machines.
+Theoretically you could probably get away with a smaller version on compute
+nodes and frontends, but the files are compared via hashing by default so just
+install identical configs everywhere.
+Must be readable by all users.
+
+### job_submit.lua
+This script is only needed on the controller, but is not sensitive so you can
+install it everywhere if that is easier.
+It does two things for our setup:
+1. A job that asks for 1 hour or less of walltime is automatically added in our
+express partition.
+2. If no specific stdout/stderr names have been asked for we set it to
+"jobname"-"jobid".out in stead of the standard slurm-"jobid".out.
+
+Must be readable by the user that slurmctld runs under.
+
+### slurmdbd.conf
+This is the config for the accounting module. Since it has the password and
+user for the database it is important that it is not accessible to regular
+users.
+
+Must be readable by the user that slurmdbd runs under.
+
+Misc
+----
+The `slurm_ld.conf` files is put into `/etc/ld.so.conf.d/` to make sure the
+binaries can find the libraries they need.
---
+++ config/job_submit.lua
@@ -0,0 +1,105 @@
+--[[
+
+ Example lua script demonstrating the SLURM job_submit/lua interface.
+ This is only an example, not meant for use in its current form.
+
+ Leave the function names, arguments, local varialbes and setmetatable
+ set up logic in each function unchanged. Change only the logic after
+ the line containing "*** YOUR LOGIC GOES BELOW ***".
+
+ For use, this script should be copied into a file name "job_submit.lua"
+ in the same directory as the SLURM configuration file, slurm.conf.
+
+--]]
+
+function _build_part_table ( part_list )
+    local part_rec = {}
+    for i in ipairs(part_list) do
+        part_rec[i] = { part_rec_ptr=part_list[i] }
+        setmetatable (part_rec[i], part_rec_meta)
+    end
+    return part_rec
+end
+
+--########################################################################--
+--
+--  SLURM job_submit/lua interface:
+--
+--########################################################################--
+
+function slurm_job_submit ( job_desc, part_list, submit_uid )
+    setmetatable (job_desc, job_req_meta)
+    local part_rec = _build_part_table (part_list)
+
+--      *** YOUR LOGIC GOES BELOW ***
+    --[[
+    if job_desc.pn_min_memory == slurm.NO_VAL then                   
+        log_user(" ** ERROR ** You _must_ specify required memory")
+        return slurm.ERROR
+    end --]]
+    -- if less than an hour is requested, add the express partition as a
+    -- possibility
+    if (job_desc.partition == nil or job_desc.partition == "normal")
+        and job_desc.time_limit ~= nil
+        and job_desc.time_limit <= 60
+        then
+            log_info("slurm_job_submit: job from uid %d, add 'express'", job_desc.user_id)
+            job_desc.partition = "express,normal"
+        end
+
+    if job_desc.std_out == nil and job_desc.std_err == nil and job_desc.name ~= nil
+        then
+            job_desc.std_out = job_desc.name .. "-%j.out"
+            job_desc.std_err = job_desc.name .. "-%j.out"
+        end
+
+        return 0
+end
+
+function slurm_job_modify ( job_desc, job_rec, part_list, modify_uid )
+    setmetatable (job_desc, job_req_meta)
+    setmetatable (job_rec,  job_rec_meta)
+    local part_rec = _build_part_table (part_list)
+
+--      *** YOUR LOGIC GOES BELOW ***
+
+    return 0
+end
+
+--########################################################################--
+--
+--  Initialization code:
+--
+--  Define functions for logging and accessing slurmctld structures
+--
+--########################################################################--
+
+
+log_info = slurm.log_info
+log_verbose = slurm.log_verbose
+log_debug = slurm.log_debug
+log_err = slurm.error
+log_user = slurm.log_user
+
+job_rec_meta = {
+    __index = function (table, key)
+        return _get_job_rec_field(table.job_rec_ptr, key)
+    end
+}
+job_req_meta = {
+    __index = function (table, key)
+        return _get_job_req_field(table.job_desc_ptr, key)
+    end,
+    __newindex = function (table, key, value)
+        return _set_job_req_field(table.job_desc_ptr, key, value or "")
+    end
+}
+part_rec_meta = {
+    __index = function (table, key)
+        return _get_part_rec_field(table.part_rec_ptr, key)
+    end
+}
+
+log_info("initialized")
+
+return slurm.SUCCESS
